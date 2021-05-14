5927b095c6957887f2bfb3fe1b012cb8b83d8140
Fabricio Dematte <fabriciodematte7p@gmail.com>
2021-05-13 21:43:47 -0300

    feat: request price ready

--- src/fetch.rs
+++ src/fetch.rs
@@ -1,1 +1,21 @@
-use reqwest::Client;
+use reqwest::blocking::get;
+
+use serde_derive::Deserialize;
+
+#[derive(Deserialize, Debug)]
+pub struct Payload {
+    pub id: String,
+    pub price: String,
+}
+
+pub fn fetch_price(coin_id: &str) -> Result<Vec<Payload>, reqwest::Error> {
+    let endpoint = format!(
+        "{}/currencies/ticker?key={}&ids={}",
+        dotenv!("API_URL"),
+        dotenv!("API_KEY"),
+        coin_id
+    );
+
+    let response: Vec<Payload> = get(endpoint)?.json()?;
+    Ok(response)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,31 @@
 extern crate clap;
+extern crate dotenv;
 extern crate reqwest;
+extern crate serde;
 extern crate serde_json;
 
+#[macro_use]
+extern crate dotenv_codegen;
+
 mod app;
+mod fetch;
+
+use dotenv::dotenv;
 
 fn main() {
+    dotenv().ok();
+
     let matches = app::AppRuntime.build().get_matches();
-    todo!("{:?}", matches)
+    let coin_id = matches.value_of("COIN").expect("No coin id provided");
+
+    let data = fetch::fetch_price(coin_id).expect("Failed to fetch data");
+
+    if data.is_empty() {
+        println!("Invalid coin ID");
+    } else {
+        let payload = data.first().unwrap();
+        let price = payload.price.parse::<f32>().unwrap();
+
+        println!("Price of {} is currently: ${:.2}", &payload.id, price);
+    }
 }
